aa0769eae4d8463eef5f48503e1e4ef964d68bec
Devin <zxcv4215240@163.com>
2015-12-11 15:49:33 +0800

    first commit

---
+++ Animation3DRolling/3DRollingView.h
@@ -0,0 +1,13 @@
+//
+//  3DRollingView.h
+//  Animation3DRolling
+//
+//  Created by Devin on 15/12/11.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _DRollingView : UIView
+
+@end
---
+++ Animation3DRolling/3DRollingView.m
@@ -0,0 +1,21 @@
+//
+//  3DRollingView.m
+//  Animation3DRolling
+//
+//  Created by Devin on 15/12/11.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import "3DRollingView.h"
+
+@implementation _DRollingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
